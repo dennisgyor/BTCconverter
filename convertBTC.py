3e4ad7c22ec57861d11f9ce4b7a07223c5ee83c6
bhuddamonk <dennisgyor@gmail.com>
2017-09-10 21:16:15 -0500

    Fixed/added comments on the code.

--- convertBTC.py
+++ convertBTC.py
@@ -2,13 +2,12 @@
 import argparse
 from forex_python.bitcoin import BtcConverter
 
+#CreaTE a new parser object and add command line arguments
 parser = argparse.ArgumentParser(description='Convert currency to BTC.')
 parser.add_argument('-c', dest='currency', type=str, default='USD', help='Enter type of currency to convert.')
 parser.add_argument('-a', dest='amount', type=float, help='Enter amount of currency to convert.')
 
 args = parser.parse_args()
-
-# usdamount = 400
 
 # create a new converter object
 b = BtcConverter()
@@ -16,7 +15,6 @@ b = BtcConverter()
 x = b.get_latest_price(args.currency)
 print("The latest price in {} for 1.0 Bitcoin (BTC) is {}".format(args.currency, str(x)))
 
-
-# convert USD to BTC
+# convert to BTC
 a = b.convert_to_btc(args.amount, args.currency)
 print("Converting {} to BTC. {} {} is currently worth {} BTC!".format(args.currency, args.amount, args.currency, str(a)))
